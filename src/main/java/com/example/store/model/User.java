9e9a78680713736b268ae09504148d86361fb336
Victor Pena <victor.javier.pena210@gmail.com>
2026-01-17 17:30:28 -0600

    model: Product

---
+++ src/main/java/com/example/store/model/User.java
@@ -0,0 +1,55 @@
+package com.example.store.model;
+
+public class User {
+	private long id;
+	private String email;
+	private String passwordHash;
+	private String fullName;
+	
+	public User() {
+		
+	}
+	
+	public User(long id, String email, String passwordHash, String fullName) {
+		this.id = id;
+		this.email = email;
+		this.passwordHash = passwordHash;
+		this.fullName = fullName;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+
+	
+	
+}
